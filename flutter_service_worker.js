47428cf2cf047aedd589677ca95fbca5f2e3865d
Shashank <59696365+shashank1q@users.noreply.github.com>
2024-06-20 18:46:53 +0530

    init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9a62a515a4a7074dd353a2fc475b8796
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "7d6523d4d8c40e5bec42a54c74e138e2",
+"flutter_bootstrap.js": "f7c2ad9263343e89eaf4ca23b2053497",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
